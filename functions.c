45cae0b1de93701fd8bbf304e99a7e75665f2be1
swathi <swathign1995@gmail.com>
2021-07-21 08:30:04 +0530

    adding more information about feature

--- functions.c
+++ functions.c
@@ -3,4 +3,7 @@
 void call_function() {
   printf("Hello swati!\n");
   printf("welcome to c program!\n");
+  printf("Hello sgn!\n");
+  printf("welcome to c program!\n");
+  printf("what do you know!\n");
 }
